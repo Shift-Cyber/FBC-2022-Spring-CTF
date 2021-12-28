d97151a3796ec43c6e8f5fcb8a74f1272091f4a9
Nathaniel Singer <nathaniel@singer.cloud>
2021-12-27 22:26:06 -0800

    tcp1234-socket-ynetd_done

---
+++ challenges/tcp1234-socket-ynetd/fast_and_furious.py
@@ -0,0 +1,13 @@
+import sys
+
+print("You have 5 seconds to count to 100. Don't mess up.\n")
+
+for i in range(100):
+    io = input(f"Send me the number {i}: ")
+
+    if not io == str(i):
+        print("Thats not what I asked for!!")
+        exit()
+
+with open("flag.txt","r") as f:
+    for l in f: print(l)
---
+++ challenges/tcp1234-socket-ynetd/timeout_stager.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+timeout 5s python3 fast_and_furious.py
