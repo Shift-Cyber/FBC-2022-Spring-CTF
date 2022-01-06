9cff524af98b4a4822f0c256d5f0aef55bf92b96
Nathaniel Singer <nathaniel@singer.cloud>
2022-01-05 23:36:39 -0800

    all challenges written

--- challenges/tcp9001-webapp-nodejs/app.js
+++ challenges/tcp9001-webapp-nodejs/app.js
@@ -1,39 +1,69 @@
-const express = require("express")
-const app = express()
 const fs = require('fs')
-const url = require('url')
+const express = require("express")
+const jwt = require('jsonwebtoken')
+const app = express()
 const port = 9001
 
 //Expose necessary directories 
-app.use('/images', express.static(__dirname + '/images'));
+app.use('/media', express.static(__dirname + '/media'));
 app.use('/public', express.static(__dirname + '/public'));
-app.use('/secret-page', express.static(__dirname + '/secret-page'));
+
+//Use JSON for processing
+app.use(express.json())
+
+// Create token with a very long expiration
+const token = jwt.sign(
+{ user: "administrator", admin: "no" },
+    "1488e16f60c15565ad989f924a99e3a0",
+    {
+        expiresIn: "1000d",
+        issuer: "Fallbrook Cyber (FBC)",
+    }
+);
+console.log(`JWT Initalized: ${token}\n`)
 
 //Redirect base to index.html with GET param
 app.get('/', function(req, res) { 
-    res.status(302).redirect("/index.html?image=robot-1"); 
+    res.status(302).redirect("/login.html"); 
 })
 
 //Serve request based on GET params
-app.get("/index.html", (req,res) => {
+app.get("/login.html", (req,res) => {
     res.writeHead(200, { "Content-Type": "text/html" })
-    fs.readFile("index.html", function(error, data) {
-        const params = url.parse(req.url, true).query;
-
+    fs.readFile("login.html", function(error, data) {
         if (error) {
             res.writeHead(404)
             res.write("File not found!")
         } else { //standard responses
-            if (['robot-1', 'robot-2', 'robot-3'].includes(params.image)) {
-                res.write(data)
-            } else if (params.image == 'flag' && params.authorized == 'yes') { // pay the prize
-                res.write(data)
-            } else {
-                res.write("Unauthorized or unknown!")
-            }
+            res.write(data)
         }
         res.end()
     })
+})
+app.get("/admin.html", (req,res) => {
+    recv_token = req.headers.authorization.split(' ').pop();
+    recv_token_decoded = jwt.decode(recv_token) // notice this is decoded and not signed
+
+    if (recv_token_decoded.user === "administrator" && recv_token_decoded.admin === "yes") {
+        // token forged, pay the prize
+        res.writeHead(200)
+        res.write("FBC{gimme_that_real_john_hancock}")
+        res.send()
+    } else { // invalid token
+        res.writeHead(401).send()
+    }
+    res.end()
+})
+
+app.post("/login.html", (req,res) => {
+    // Validate credentials
+    if (req.body.username === "admin" && req.body.password === "monkeybusiness") {
+        res.header({ "Authorization": "Bearer " + token })
+        res.redirect(202, "/admin.html"); 
+    } else { //bad creds
+        res.writeHead(401).send()
+    }
+    res.end()
 })
 
 app.listen(port, function(error) {
